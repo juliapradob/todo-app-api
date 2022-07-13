a24e95f253eeb94cb381a1dd5ac90f5c63cfd8c8
Julia Prado <juliapbasile@gmail.com>
2022-07-13 19:36:55 -0300

    feat: model Usuarios importada no controller

--- src/controllers/Usuarios.js
+++ src/controllers/Usuarios.js
@@ -1,7 +1,9 @@
+import UsuarioModel from "../models/UsuarioModel.js"
 class Usuarios {
     static rotas(app) { //não quero estanciar, transformar num objeto, apenas usar funções dela
         app.get("/usuarios", (req, res) => {
-            res.send("rota usuários")
+            const usuario = new UsuarioModel("Julia", "julia@email.com", "11994567053")
+            res.send(usuario)
         })
     }
 }
