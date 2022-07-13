7d4fc1060445c096ea0fac0ea630a7ff9fe7c99b
Julia Prado <juliapbasile@gmail.com>
2022-07-13 19:52:45 -0300

    feat: criada service de validação de nome do usuário

--- src/controllers/Usuarios.js
+++ src/controllers/Usuarios.js
@@ -1,9 +1,16 @@
 import UsuarioModel from "../models/UsuarioModel.js"
+import ValidacoesService from "../services/ValidacoesService.js"
 class Usuarios {
     static rotas(app) { //não quero estanciar, transformar num objeto, apenas usar funções dela
         app.get("/usuarios", (req, res) => {
-            const usuario = new UsuarioModel("Julia", "julia@email.com", "11994567053")
-            res.send(usuario)
+            const nome = "Julia"
+            if (ValidacoesService.validaNome(nome)) {
+                const usuario = new UsuarioModel("Julia", "julia@email.com", "11994567053")
+                res.send(usuario)
+            } else {
+                res.status(400).send("Erro")
+            }
+            // res.send("rota usuários")
         })
     }
 }
---
+++ src/services/ValidacoesService.js
@@ -0,0 +1,7 @@
+class ValidacoesService {
+    static validaNome(nome) {
+        return nome.length >= 2
+    }
+}
+
+export default ValidacoesService
