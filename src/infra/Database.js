9d2cf331d2105d296862ff73c3a1789659960f00
Julia Prado <juliapbasile@gmail.com>
2022-07-26 19:11:56 -0300

    feat: troca dos métodos de array para sqlite, com inclusão de tabelas e população

--- src/infra/Database.js
+++ src/infra/Database.js
@@ -1,30 +1,9 @@
-export const Database = {
-    Usuarios: [
-        {
-            nome: "Vitor",
-            email: "oi@oi.com",
-            telefone: "123456789"
-        },
-        {
-            nome: "Julia",
-            email: "oi@ola.com",
-            telefone: "123456789"
-        },
-        {
-            nome: "Karina",
-            email: "oi@ola.com",
-            telefone: "123456789"
-        },
-        {
-            nome: "Camila",
-            email: "oi@oi.com",
-            telefone: "123456789"
-        },
-        {
-            nome: "Gabriella",
-            email: "oi@ola.com",
-            telefone: "123456789"
-        },
-    ],
-    Tarefas: []
-}
+import sqlite3 from "sqlite3";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+sqlite3.verbose()
+const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db"
+const Database = new sqlite3.Database(filePath)
+
+export default Database 
--- src/utils/DatabaseMetodos.js
+++ src/utils/DatabaseMetodos.js
@@ -1,116 +1,55 @@
-import { Database } from "../infra/Database.js";
+import Database from "../infra/Database.js";
 
-class DatabaseMetodos {
-    /**
-     * 
-     * @returns Array
-     */
-    static listarTodoOBanco() {
-        return Database
-    }
+class DatabaseMetodos{
+    static activePragma(){
+        const pragma = "PRAGMA foreign_keys = ON"
 
-    /**
-     * 
-     * @returns Array<Objects>
-     */
-    static listarTodosUsuarios() {
-        return Database.Usuarios
-    }
-
-    /**
-     * 
-     * @param {number} index 
-     * @returns Elemento<Array>
-     */
-    static listarUsuarioPorIndex(index) {
-        return Database.Usuarios[index]
-    }
-
-    /**
-     * 
-     * @returns Array<object>
-     */
-    static listarEmails() {
-        return Database.Usuarios.email
-    }
-
-    /**
-     * 
-     * @param {string} email 
-     * @param {array<object>} database 
-     * @returns Array<object>
-     */
-    static filtraEmail (email, database) {
-        return database.filter((valor) => {
-            return valor.email == email
-        })
-    }
-
-    /**
-     * 
-     * @returns Array<Objects>
-     */
-    static listarTodasTarefas() {
-        return Database.Tarefas
-    }
-    /**
-     * 
-     * @param {Object} usuario 
-     * @returns Object
-     */
-    static inserirUsuario(usuario) {
-        Database.Usuarios = [...Database.Usuarios, usuario] // ou Database.Usuarios.push(usuario)
-        return Database.Usuarios
-    }
-
-    /**
-     * 
-     * @param {Object} tarefa 
-     * @returns Object
-     */
-    static inserirTarefa(tarefa) {
-        Database.Tarefas = [...Database.Tarefas, tarefa] // ou Database.Tarefas.push(tarefa)
-        return Database.Tarefas
-    }
-
-    /**
-     * 
-     * @param {number} id 
-     * @returns Array<object>
-     */
-    static deletaUsuarioPorId(id) {
-        const usuarios = Database.Usuarios.filter((usuario, index) => {
-            return id != index
-        })
-        Database.Usuarios = usuarios
-        return {id: id, success: "Usuário excluído com sucesso"}
-    }
-
-    /**
-     * 
-     * @param {number} id 
-     * @param {object} usuario 
-     * @returns Array<object>
-     */
-    static atualizarPorId(id, usuario) {
-        const newUsuario = Database.Usuarios.map((usuario, index) => {
-            if (index == id) {
-                return usuario
+        Database.run(pragma, (e)=>{
+            if(e){
+                console.log(e)
+            } else {
+                console.log("Chaves estrangeiras estão ativas")
             }
         })
-        Database.Usuarios = newUsuario
-        return {success: "Usuário atualizado", id: id}
     }
 
-    static atualizarPorEmail(email, usuario) {
-        const newUsuario = Database.Usuarios.map((emailOriginal) => {
-            if (emailOriginal.email == email) {
-                return usuario
-            }
-            return emailOriginal
+    static createTable(){
+        
+        this.activePragma()
+
+        const query = `
+        CREATE TABLE IF NOT EXISTS usuarios(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR,
+            email VARCHAR,
+            telefone VARCHAR
+        )
+        `
+        return new Promise((resolve, reject)=>{
+            Database.run(query, (e)=>{
+                if(e){
+                    reject(e.message)
+                } else {
+                    resolve("Tabela usuários criada com sucesso!")
+                }
+            })
         })
-        Database.Usuarios = newUsuario
-        return {success: "Email atualizado", email: email}
+    }
+
+    static inserirUsuario(usuario){
+        const query = `INSERT INTO usuarios (nome, email, telefone) VALUES (?,?,?)`
+
+        const body = Object.values(usuario)
+
+        return new Promise((resolve, reject)=>{
+            Database.run(query, [...body], (e)=>{
+                if(e){
+                    reject(e.message)
+                } else {
+                    resolve({error: false, message: "Usuario cadastrado com sucesso!"})
+                }
+            })
+        })
     }
 }
 
